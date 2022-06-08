2cc42e22be19c36bba0f529fd50dcbf421270a73
charlesxu27 <charlesxu27@gmail.com>
2022-06-08 20:49:48 +0200

    Add DOM references

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// DOM references
+searchFormElement = document.querySelector("form")
+searchInputElement = document.querySelector("#search")
+searchButtonElement = document.querySelector("#submit-button")
+gifDivElement = document.querySelector("#gif-div")
